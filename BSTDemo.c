60e291050404dd73746ad6b72b342ed7f3975775
Stefany Carrillo <scarrillo7@miners.utep.edu>
2017-02-28 16:00:02 -0700

    testing per node

--- BSTDemo.c
+++ BSTDemo.c
@@ -3,17 +3,25 @@
 
 
 int main(void){
-  FILE *filePointer = fopen("names.txt", "r");
+/*  FILE *filePointer = fopen("names.txt", "r");
   char input[100] = "";
   BSTree *employees = createTree();
 
   if(filePointer == NULL)
     printf("File not found.");
+  
+  */
 
-  while(fgets(input, 100, filePointer)){
-    printf(input);
-    insertName(&employees, input);
-  }//end while
+    BSTNode *temptree = createNode("b");
+    insertNode(temptree, "a");
+    printNode(temptree);
+    
+    //printf("in main: %s \n",temptree->right->name);
+
+//  while(fgets(input, 100, filePointer)){
+//    printf(input);
+//    insertName(&employees, input);
+//  }//end while
 
   //printTree(&employees);
   //printf("\n\nEnd of Program\n");
