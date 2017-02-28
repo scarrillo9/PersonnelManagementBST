a21a1ea716f9e5ddcaf524610bf81c7a2b40d3a1
Stefany Carrillo <scarrillo7@miners.utep.edu>
2017-02-28 16:00:57 -0700

    added insertNode method to test per node

--- BSTree.c
+++ BSTree.c
@@ -16,46 +16,62 @@ BSTree *createTree(){
 
 BSTNode *createNode(char *name){
   BSTNode *newNode = (BSTNode *)malloc(sizeof(BSTNode));
-  newNode->name = name;
+  newNode->name = copyName(name);
   newNode->left = newNode-> right = NULL;
   //doCheck(tree);
   return newNode;
 }//end createNode
 
-void freeTree(BSTree **tree){
-  //doCheck(tree);
-  BSTree *this_tree = *tree;
-  bstFree(&this_tree);
-  free(tree);
-}//end freeTree
+char *copyName(char *name){
+  char *copy = malloc(strlen(name+1));
+  if(copy != NULL){
+    strcpy(copy, name);
+  }
+  return copy;
+}//end copyName
 
-void bstFree(BSTree **tree){
-  BSTree *this_tree = *tree;
-  BSTNode *current = this_tree->root, *right, *left;
-  left = current->left;
-  //doCheck(tree);
+void insertNode(BSTNode *root, char *newName){
+    char *scopy = copyName(newName);
+    scopy[strlen(scopy)] = '\0';
+    
+    if(!root){
+        root = createNode(scopy);
+        printf("in if %s\n", root->name);
+    }
+    
+    else{
+        printf("in else \n");
+        int i = 0;
 
-  while(current){
-    right = current->right;
-    free(current->name);
-    free(current);
-    current = right;
-  }//end while
+        while(root->name[i] || scopy[i]){
+	  if(root->name[i] > scopy[i]){
+	    insertNode(root->left, scopy);
+	    return;
+	  }//end if
 
-  current = left;
-  while(current){
-    left = current->left;
-    free(current->name);
-    free(current);
-    current = left;
-  }//end while
+          if(root->name[i] < scopy[i]){
+	    insertNode(root->right, scopy);
+	    return;
+	  }//end if
 
-  this_tree->root = NULL;
-}//end bstFree
+          else
+            i = i + 1;
+        }//end while
+
+        if(root->name[i] == '\0'){
+          insertNode(root->right, scopy);
+	}//end if
+        if(newName[i] == '\0'){
+	  insertNode(root->left, scopy);
+	}//end if
+        
+    }
+}//end insertNode
 
 void insertName(BSTree **tree, char *newName){
   BSTree *this_tree = *tree;
   if(this_tree->root == NULL){
+    printf("in if");
     this_tree->root = createNode(newName);
     return;
   }//end if
@@ -87,6 +103,14 @@ void insert(BSTNode **root, char *newName){
     insert(&(this_root->left), newName);
 }//end insert
 
+void printNode(BSTNode *node){
+    if(node){
+        if(node->left) printNode(node->left);
+        printf("in printNode method: %s\n", node->name);
+        if(node->right) printNode(node->right);
+    }
+}
+
 void printTree(BSTree **tree){
   BSTree *this_tree = *tree;
   if(this_tree->root == NULL)
@@ -105,6 +129,37 @@ void print(BSTNode **root){
   if(this_root->right)
     print(&(this_root->right));
 }//end print
+
+void freeTree(BSTree **tree){
+  //doCheck(tree);
+  BSTree *this_tree = *tree;
+  bstFree(&this_tree);
+  free(tree);
+}//end freeTree
+
+void bstFree(BSTree **tree){
+  BSTree *this_tree = *tree;
+  BSTNode *current = this_tree->root, *right, *left;
+  left = current->left;
+  //doCheck(tree);
+
+  while(current){
+    right = current->right;
+    free(current->name);
+    free(current);
+    current = right;
+  }//end while
+
+  current = left;
+  while(current){
+    left = current->left;
+    free(current->name);
+    free(current);
+    current = left;
+  }//end while
+
+  this_tree->root = NULL;
+}//end bstFree
 
 /*
 int llCheck(BSTree *tree){
