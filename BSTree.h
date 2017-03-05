b2bfd664b1797f4263fdf3afaab170717d6c63c4
Stefany Carrillo <scarrillo7@miners.utep.edu>
2017-03-05 16:13:28 -0700

    added new methods from demo and tree files

--- BSTree.h
+++ BSTree.h
@@ -11,35 +11,40 @@ typedef struct BSTree{
 } BSTree;
 
 
-//allocates memory for a new tree
+//BSTDemo.c methods
+void menu(BSTree *employees);
+
+void methods(BSTree *employees, char option);
+
+void makeFile(BSTree *employees);
+
+
+//BSTree.c methods
+//allocates memory for a new tree, nodes and names
 BSTree *createTree();
 
 BSTNode *createNode(char *name);
 
 char *copyName(char *name);
 
-
-
+//insert method
 BSTNode *insertNode(BSTNode *root, char *ncopy);
 
-//BSTree *insertName(BSTree *tree, char *newName);
-
-
-
+//print tree methods
 void printNode(BSTNode *node);
+
+void inputNamesToFile(BSTNode *root, FILE *file);
 
 void printTree(BSTree *tree);
 
-
+//delete methods
 BSTNode *delete(BSTNode *root, char *name);
 
 BSTNode *minimumEmployee(BSTNode *root);
 
-//free allocated memory
+//free allocated memory methods
 void freeTree(BSTree *tree);
 
 void bstFree(BSTNode *tree);
-
-
 
 #endif
